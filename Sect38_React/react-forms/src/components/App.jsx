e5a7b656877034d7df7236fc1856443fa8ac2f00
Japneet Singh <japneetsingh@gmail.com>
2023-06-12 23:45:39 -0700

    changing complex state

--- Sect38_React/react-forms/src/components/App.jsx
+++ Sect38_React/react-forms/src/components/App.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+export default function App() {
+    const [name, setName] = useState("");
+    const [inputText, setInputText] = useState("");
+
+    function handleChange(event) {
+        setInputText(event.target.value);
+    }
+
+    function handleClick(event) {
+        setName(inputText);
+        setInputText("");
+        event.preventDefault();
+    }
+
+    return (
+        <div className="container">
+            <h1>Hello {name}</h1>
+            <form onSubmit={handleClick}>
+                <input
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="What's your name?"
+                    value={inputText}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
-
-export default App;
