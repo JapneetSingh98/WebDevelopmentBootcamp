c80702493b63c4adc034a4df6738e6de963c2ecd
Japneet Singh <japneetsingh@gmail.com>
2023-06-14 01:08:17 -0700

    javascript es6 spread operator

---
+++ Sect38_React/es6-spread-operator/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
+// const citrus = ["Lime", "lemon", "orange"];
+// const fruits = ["Apple", ...citrus, "Banana", "Coconut"];
+
+// console.log(fruits);
+
+// const fullName = {
+//     fName: "James",
+//     lName: "Bond"
+// };
+
+// const user = {
+//     ...fullName,
+//     id: 1,
+//     username: "jamesbond007"
+// };
+
+// console.log(user);
